0b552e2606e6976a35af1f4885638f0c2a68bfa4
nikitabairagi <nikitabairagi@gmail.com>
2018-11-20 16:42:49 -0800

    added accident predictions.html and modified searchbycounty file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 county_df = pd.read_csv('data/counties.csv', engine='python',sep = ',')
@@ -9,10 +9,12 @@ county = 1
 counties = []
 collisions_df = pd.read_csv('data/collisions.csv', engine='python',sep = ',')
 county_df = pd.read_csv('data/counties.csv', engine='python',sep = ',')
+
 #screen 1 get accidents count
 jsonfile=open("static/js/statesData.json")
 accidents_data=json.load(jsonfile)
 accidents_df = collisions_df[['CASE_ID','COUNTY']].groupby('COUNTY').count().reset_index()
+
 for feature in accidents_data['features']:
     idx = feature['properties']['name'].upper()
     if len(accidents_df[accidents_df.COUNTY == idx]) > 0:
@@ -26,21 +28,21 @@ for i in range(len(county_df)):
 @app.route("/home")
 
 def home():
-    return render_template('screen1.html',accidents_data = accidents_data)
+    return render_template('index.html',accidents_data = accidents_data)
 	
 @app.route("/searchbycounty")
 @app.route("/searchbycounty/dataview")
 def countydata():
     county = request.args.get('county',default = 1,type = int)
-    return render_template('countycontents.html',counties = counties, sel_county = county)
+    return render_template('searchbycounty-dataview.html',counties = counties, sel_county = county)
 
 @app.route("/searchbycounty/mapview")
 def countymap():
-    return render_template('countycontents.html',counties = counties, sel_county = county)
+    return render_template('searchbycounty-mapview.html',counties = counties, sel_county = county)
 
-@app.route("/predictaccidents")
+@app.route("/accidents-prediction")
 def predict():
-    return render_template('PredictAccidents.html')
+    return render_template('accidentsPrediction.html')
 	
 @app.route("/login")	
 def login():
