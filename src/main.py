5e8213899fd53a86b7cd85608c52027fb01f3689
vandana414 <vandanakudigrama.shenoy@sjsu.edu>
2018-11-27 12:41:39 -0800

    correcting error

--- src/main.py
+++ src/main.py
@@ -118,7 +118,7 @@ def countymap():
 
 @app.route("/accidents-prediction")
 def predictaccidents():
-    return render_template('accidentsPrediction.html',coordinates = json.dumps(coordinates),,currentdttm=currentdttm)
+    return render_template('accidentsPrediction.html',coordinates = json.dumps(coordinates),currentdttm=currentdttm)
 
 @app.route("/logout")
 def logout():
